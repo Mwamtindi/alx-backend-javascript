82e0cb3e0f3082fd045e94f5b790dfe37740a4a9
Mwamtindi <mwamtindishaban@gmail.com>
2024-10-03 22:23:39 +0300

    implement a class named Car

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -4,24 +4,25 @@ const _color = Symbol('color');
 
 export default class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this._brand = brand; // Store the brand in a regular attribute
+    this._motor = motor; // Store the motor in a regular attribute
+    this._color = color; // Store the color in a regular attribute
   }
 
   get brand() {
-    return this[_brand];
+    return this._brand;
   }
 
   get motor() {
-    return this[_motor];
+    return this._motor;
   }
 
   get color() {
-    return this[_color];
+    return this._color;
   }
 
   cloneCar() {
-    return new this.constructor(this.brand, this.motor, this.color);
+    // Create a new instance using the same constructor
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
