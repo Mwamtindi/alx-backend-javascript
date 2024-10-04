0f7759b024bbfd43837a29ab6a3810efcf98be58
Mwamtindi <mwamtindishaban@gmail.com>
2024-10-04 21:34:28 +0300

    handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,15 +2,13 @@ import signUpUser from './4-user-promise'; // import from 4-user-promise.js
 import uploadPhoto from './5-photo-reject'; // import from 5-photo-reject.js
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [];
-  try {
-    const user = await signUpUser(firstName, lastName);
-    promises.push({ status: 'fulfilled', value: user });
-    await uploadPhoto(fileName);
-  } catch (error) {
-    promises.push({
-      status: 'rejected',
-      value: `Error: ${fileName} cannot be processed`,
-    });
-  }
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+
+  return Promise.allSettled(promises).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
 }
