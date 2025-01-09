9d1ca57e8d611cf83f63004b96c36f2e2f23794b
Mwamtindi <mwamtindishaban@gmail.com>
2025-01-09 11:34:08 +0300

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * Prints a strng to the console (STDOUT).
+ * @param {strng} messge - The string to print.
+ */
+
+function displayMessage(message) {
+    console.log(message);
+}
+
+module.exports = displayMessage;
